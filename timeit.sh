0ecebe7518ab17b2441b8610b566ae8d1e7c0f9b
Jakukyo Friel <weakish@gmail.com>
2016-05-07 10:41:43 +0800

    Add time script.

---
+++ timeit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+for i in bin/*; do
+    echo $(basename $i) >> footprint.txt
+    time -a -o footprint.txt ./$i
+done
+
+echo 'Ceylon' >> footprint.txt
+time -a -o footprint.txt ceylon run default
+echo 'Kotlin' >> footprint.txt
+time -a -o footprint.txt java -jar hello.jar
+du -h hello.jar >> footprint.txt
+echo 'Swift' >> footprint.txt
+time -a -o footprint.txt ./hello-swift
+echo 'Go' >> footprint.txt
+time -a -o footprint.txt ./hello-go
+du -h hello-go >> footprint.txt
+echo 'Haskell' >> footprint.txt
+time -a -o footprint.txt ./hello-hs
+du -h hello-hs >> footprint.txt
+echo 'OCaml' >> footprint.txt
+time -a -o footprint.txt ./hello-ml
+du -h hello-ml >> footprint.txt
