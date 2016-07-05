00e2e630f5df5a263476294088be6a378f35ad2f
Jakukyo Friel <weakish@gmail.com>
2016-07-05 22:36:46 +0800

    Swift: use static linked stdlib.

--- compileit.sh
+++ compileit.sh
@@ -1,6 +1,6 @@
 ceylon compile source/hello.ceylon
 kotlinc -include-runtime hello.kt -d hello.jar
-swiftc -gnone -O hello.swift -o hello-swift
+swiftc -static-stdlib -gnone -O hello.swift -o hello-swift
 go build -o hello-go hello.go
 ghc -o hello-hs hello.hs
 ocamlopt -o hello-ml hello.ml
