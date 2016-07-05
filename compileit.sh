fe2457b79cc31047ea1bf89682e086fe80d16db6
Jakukyo Friel <weakish@gmail.com>
2016-07-05 22:31:26 +0800

    Use ocamlopt instead of ocamlc.

--- compileit.sh
+++ compileit.sh
@@ -3,4 +3,4 @@ kotlinc -include-runtime hello.kt -d hello.jar
 swiftc -gnone -O hello.swift -o hello-swift
 go build -o hello-go hello.go
 ghc -o hello-hs hello.hs
-ocamlc -o hello-ml hello.ml
+ocamlopt -o hello-ml hello.ml
